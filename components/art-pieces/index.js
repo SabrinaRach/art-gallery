ee0dc6736349bb7e64baec59ae19f6864fe08086
Sabrina <sabrinarach.sr@gmail.com>
2026-08-05 12:07:26 +0200

    create two components: art piece and art piece preview

---
+++ components/art-pieces/index.js
@@ -0,0 +1,13 @@
+
+
+return (
+  <div className="gallery-page">
+    <h1>Gallery Page</h1>
+  </div>
+    <li>
+        title={piece.name}
+        image={piece.imageSource}
+        artist={piece.artist}
+        slug={piece.slug} 
+    </li>
+);
