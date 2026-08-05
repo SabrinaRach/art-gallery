e2f8afd784e8a7a1570264c843d38c48fb63e0ed
Sabrina <sabrinarach.sr@gmail.com>
2026-08-05 14:59:31 +0200

    add comments to better understand the code

--- components/art-pieces/index.js
+++ components/art-pieces/index.js
@@ -1,22 +1,25 @@
-import ArtPiecePreview from '../art-piece-preview';
+import ArtPiecePreview from "../art-piece-preview"; /* importing ArtPiecePreview component from the art-piece-preview directory. This component is used to display a preview of an individual art piece. */
 
-export default function ArtPieces({ pieces }) {
-return (
-  <div className="gallery-page">
-    <h1>Gallery Page</h1>
-  </div>
-  <ul>
-  {pieces.map((piece) => (
-    <li key={piece.slug}>
-        <ArtPiecePreview
-          title={piece.name}
-          image={piece.imageSource}
-          artist={piece.artist}
-          slug={piece.slug}
-        />
-    </li>
-  ))}
- </ul>
-
-);
+/* Renders the gallery page.
+The "artPieces" array is passed as a prop from the parent component. */
+export default function ArtPieces({ artPieces }) {
+  return (
+    <div className="gallery-page">
+      <h1>Gallery Page</h1>
+      {/* Maps through the artPieces array and creates a preview for each artwork */}
+      <ul>
+        {artPieces.map((piece) => (
+          <li key={piece.slug}>
+            {/* Displays a preview of the artwork. The artwork data is passed as props*/}
+            <ArtPiecePreview
+              title={piece.name}
+              image={piece.imageSource}
+              artist={piece.artist}
+              slug={piece.slug}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
