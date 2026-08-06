4513ed1c8fca21859f3835cbf6e5974432afafb2
SaskiaWeiß1234 <saskia.weiss@bstableau.de>
2026-08-06 11:16:21 +0200

    changed next.config, resolved fetching error

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
-import GlobalStyle from "../styles";
+
+import useSWR from "swr";
+
+/* ---- FETCH DATA FROM API ---*/
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  /* error handling */
+  if (error) return <div>Failed to load art pieces</div>;
+  if (isLoading) return <div>Loading...</div>;
+ console.log("SWR:", { data, error, isLoading });
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      
+      <Component {...pageProps} artPieces={data} />
     </>
   );
 }
--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+
 import ArtPieceDetails from '../../components/ArtPieceDetails';
 
 export default function ArtPiecePage({ artPieces }) {
